aa6999c278afa72aca3e78e4bb9a9e6f4c5927c5
OniityanH <907209562@qq.com>
2017-11-29 18:53:27 +1030

    add more but it cannot been tested

--- HatH.py
+++ HatH.py
@@ -11,16 +11,19 @@ br.submit_form(form)
 br.open("https://e-hentai.org/hentaiathome.php")
 src = str(br.parsed()) #store all the htmlpage into src
 
+#get status
 StatusStart = '<td style="font-weight:bold; color:green">'
 StatusEnd = '</td>'
 Status = re.search('%s(.*)%s' % (StatusStart, StatusEnd), src).group(1)
 print(Status)
 
+#get last seen time
 LastSeenStart = '</td>\n<td>2016-10-16</td>\n<td>'
 LastSeenEnd = '</td>'
 LastSeen = re.search('%s(.*)%s' % (LastSeenStart, LastSeenEnd), src).group(1)
 print(LastSeen)
 
+#get file served number
 FilesServedStart = '</td>\n<td>'
 FilesServedEnd = '</td>\n<td style="text-align:left; padding-left:7px">'
 FilesServed = re.search(LastSeen + '%s(.*)%s' % (FilesServedStart, FilesServedEnd), src).group(1)
@@ -33,12 +36,31 @@ for element in FilesServed:
 print(FilesServedstr)
 print(int(FilesServedstr))
 
+#get ip
 ClientIPStart = '</td>\n<td style="text-align:left; padding-left:7px">'
 ClientIPEnd = '</td>'
 ClientIP = re.search('%s(.*)%s' % (ClientIPStart, ClientIPEnd), src).group(1)
 print(ClientIP)
 
+#get maxspeed
+Max_SpeedStart = '</td>\n<td>1.4.2 Stable</td>\n<td>'
+Max_SpeedEnd = '</td>\n<td style="color:green">'
+Max_Speed = re.search('%s(.*)%s' % (Max_SpeedStart, Max_SpeedEnd), src).group(1)
+print(int(Max_Speed))
+
+#get trust
+TrustStart = 'KB/s</td>\n<td style="color:green">'
+TrustEnd = '</td>'
+Trust = re.search('%s(.*)%s' % (TrustStart, TrustEnd), src).group(1)
+print(Trust)
+
+#get Quality
+QualityStart = Trust + '</td>\n<td>'
+QualityEnd = '</td>'
+Quality = re.search('%s(.*)%s' % (QualityStart, QualityEnd), src).group(1)
+print(int(Quality))
 
 
 
-#Your IP address has been temporarily banned for excessive pageloads which indicates that you are using automated mirroring/harvesting software. The ban expires in 33 minutes and 26 seconds
+#Your IP Max_Speed has been temporarily banned for excessive pageloads which indicates that you are using automated mirroring/harvesting software. The ban expires in 33 minutes and 26 seconds
+#Your IP address has been temporarily banned for excessive pageloads which indicates that you are using automated mirroring/harvesting software. The ban expires in 23 hours and 16 minutes
